97e7c561b2bc6d1b5d658cfabd6b416b5bb21b0f
NiemannLouise <Niemannlouise@gmail.com>
2015-05-08 09:31:54 +0200

    Car Klassen m. Getters

--- 04-Implementation/FerrariFinanceSystem/src/domain/Car.java
+++ 04-Implementation/FerrariFinanceSystem/src/domain/Car.java
@@ -2,4 +2,15 @@ package domain;
 
 public class Car {
 
+	private int serialnumber;
+	private double price;
+	
+	public int getSerialnumber() {
+		return serialnumber;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
 }
