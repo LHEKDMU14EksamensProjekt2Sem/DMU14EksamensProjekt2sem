a91999840d8b6b22ec6af67ba99f4700fdfa18f0
NiemannLouise <Niemannlouise@gmail.com>
2015-05-08 09:40:08 +0200

    CarModel Getters

--- 04-Implementation/FerrariFinanceSystem/src/domain/CarModel.java
+++ 04-Implementation/FerrariFinanceSystem/src/domain/CarModel.java
@@ -2,4 +2,15 @@ package domain;
 
 public class CarModel {
 
+	private String model;
+	private double price;
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
 }
