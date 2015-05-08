05ec9ea10458ac1399541d64fc216c8abb14a099
NiemannLouise <Niemannlouise@gmail.com>
2015-05-08 09:47:23 +0200

    Salesman getters

--- 04-Implementation/FerrariFinanceSystem/src/domain/Salesman.java
+++ 04-Implementation/FerrariFinanceSystem/src/domain/Salesman.java
@@ -1,10 +1,27 @@
 package domain;
 
 public class Salesman {
-  private int id;
-  private String navn;
-  private String username;
-  private int level;
-  
-  
+
+	private int id;
+	private String navn;
+	private String username;
+	private int level;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+
 }
