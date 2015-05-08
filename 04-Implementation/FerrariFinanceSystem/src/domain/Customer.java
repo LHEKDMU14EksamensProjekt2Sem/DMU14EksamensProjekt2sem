daa4fcffccf96c2db80115dc4515c53c1f71f9eb
NiemannLouise <Niemannlouise@gmail.com>
2015-05-08 09:42:55 +0200

    Customer getters and setters

--- 04-Implementation/FerrariFinanceSystem/src/domain/Customer.java
+++ 04-Implementation/FerrariFinanceSystem/src/domain/Customer.java
@@ -2,4 +2,95 @@ package domain;
 
 public class Customer {
 
+	private int id;
+	private String cpr;
+	private String firstName;
+	private String lastName;
+	private String streetName;
+	private String number;
+	private String city;
+	private int postalCode;
+	private String phone;
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCpr() {
+		return cpr;
+	}
+
+	public void setCpr(String cpr) {
+		this.cpr = cpr;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
