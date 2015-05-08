1954827ae4d8fa6070c70dc92afa926662f5aab6
NiemannLouise <Niemannlouise@gmail.com>
2015-05-08 09:38:49 +0200

    CarConfiguration

--- 04-Implementation/FerrariFinanceSystem/src/domain/CarConfiguration.java
+++ 04-Implementation/FerrariFinanceSystem/src/domain/CarConfiguration.java
@@ -2,4 +2,20 @@ package domain;
 
 public class CarConfiguration {
 
+	private String name;
+	private String description;
+	private double price;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+		
 }
