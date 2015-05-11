9cf50a7b5fb0ec15392c76e18332fb1f96feba89
Enok Mikkelsen <enokmik@gmail.com>
2015-05-11 08:43:15 +0200

    GUI Design

---
+++ 03-Analysis-and-Design/PoC Prototypes/GUI Design/src/ui/StartFrame.java
@@ -0,0 +1,35 @@
+package ui;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class StartFrame extends JFrame {
+  
+  public static JFrame frame;
+  
+  public StartFrame() {
+    frame =new JFrame("Ferrari Finance System");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    
+    JComponent newContentPane = new Centerpane();
+    newContentPane.setOpaque(true); //content panes must be opaque
+    
+    frame.setContentPane(newContentPane);
+    frame.setLocation( 200, 100 );   
+    frame.setPreferredSize( new Dimension( 1024, 640 ) );
+    
+    ImageIcon LHEK = new ImageIcon("pic/LHEK.png");
+    frame.setIconImage(LHEK.getImage());
+    
+    frame.setResizable( false );
+    frame.pack();
+    frame.setVisible(true);
+  }
+  
+  
+  public static void update(){
+    frame.revalidate();
+    frame.repaint();
+  }
+}
