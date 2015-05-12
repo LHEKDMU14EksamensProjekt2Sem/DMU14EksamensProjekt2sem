6ed109bfddfca9310db3ea1f1776893ced6a3ccc
NiemannLouise <Niemannlouise@gmail.com>
2015-05-12 22:31:01 +0200

    Opretter klasser til Salesman

---
+++ 04-Implementation/FerrariFinanceSystem/src/dataaccess/SalesmanDataAccess.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+public class SalesmanDataAccess {
+
+	/*
+	 * I Denne klasse skal der ligge
+	 * ReadOne - Skal omdøbes senere
+	 */
+	
+	private static final String SELECT_ONE = "";
+	
+}
