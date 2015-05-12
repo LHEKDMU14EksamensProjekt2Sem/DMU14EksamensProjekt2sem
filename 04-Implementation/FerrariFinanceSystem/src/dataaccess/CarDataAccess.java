2be0461a56938633e46fd5420a0443ab5ae2a281
NiemannLouise <Niemannlouise@gmail.com>
2015-05-12 22:24:50 +0200

    Kommentarer i DataAccess-Klasser

--- 04-Implementation/FerrariFinanceSystem/src/dataaccess/CarDataAccess.java
+++ 04-Implementation/FerrariFinanceSystem/src/dataaccess/CarDataAccess.java
@@ -2,4 +2,13 @@ package dataaccess;
 
 public class CarDataAccess {
 
+	/*
+	 * I Denne klasse skal der ligge
+	 * ReadOne
+	 * ReadAll 
+	 */
+	
+	private static final String SELECT_ONE = "";
+	private static final String SELECT_ALL = "";
+	
 }
