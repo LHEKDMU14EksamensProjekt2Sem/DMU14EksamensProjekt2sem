210b6c7d2b2f678ae42f3a0816e80e79482a24da
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-05-13 00:10:33 +0200

    Added repayment plan PoC prototype

---
+++ 03-Analysis-and-Design/PoC Prototypes/RepaymentPlan Prototype/src/loan/RepaymentCalculator.java
@@ -0,0 +1,15 @@
+package loan;
+
+import java.math.BigDecimal;
+
+public class RepaymentCalculator {
+   public BigDecimal calculateMonthlyPayment(BigDecimal principal, double interestRate, int term) {
+      BigDecimal res = new BigDecimal((principal.doubleValue() * interestRate) / (1 - Math.pow(1 + interestRate, -term)));
+      res = res.setScale(2, BigDecimal.ROUND_HALF_UP);
+      return res;
+   }
+
+   public int calculateTerm(BigDecimal principal, double interestRate, BigDecimal monthlyPayment) {
+      return (int) Math.ceil(Math.log(1 / (1 - (principal.doubleValue() * interestRate) / monthlyPayment.doubleValue())) / Math.log(1 + interestRate));
+   }
+}
