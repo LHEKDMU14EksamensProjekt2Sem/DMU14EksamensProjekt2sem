2290abc014460e83c1765963b26318080f4e68b5
NiemannLouise <Niemannlouise@gmail.com>
2015-05-20 22:16:24 +0200

    CustomerDataAccess kigget på (har lidt mange spørgsmål i morgen, så fortsætter jeg :-))

--- 04-Implementation/FerrariFinanceSystem/src/data/access/CustomerAccess.java
+++ 04-Implementation/FerrariFinanceSystem/src/data/access/CustomerAccess.java
@@ -1,44 +1,81 @@
 package data.access;
 
-import domain.Customer;
-import util.jdbc.ConnectionHandler;
-
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import util.jdbc.ConnectionHandler;
+import domain.Customer;
+import domain.Person;
+
 public class CustomerAccess {
-   private ConnectionHandler con;
+	private ConnectionHandler con;
 
-   public CustomerAccess(ConnectionHandler con) {
-      this.con = con;
-   }
+	public CustomerAccess(ConnectionHandler con) {
+		this.con = con;
+	}
 
-   public void createCustomer(Customer customer) throws SQLException {
-      try (PreparedStatement st = con.get().prepareStatement(SQL.INSERT_ONE)) {
-         st.setInt(1, customer.getPerson().getId());
-         st.setBoolean(2, customer.inGoodStanding());
-         st.executeUpdate();
-      }
-   }
+	public void createCustomer(Customer customer) throws SQLException {
+		try (PreparedStatement st = con.get().prepareStatement(SQL.INSERT_ONE)) {
+			st.setInt(1, customer.getPerson().getId());
+			st.setBoolean(2, customer.inGoodStanding());
+			st.executeUpdate();
+		}
+	}
 
-   // TODO
-   public Customer readCustomer(int id) throws SQLException {
-      return null;
-   }
+	// TODO
+	public Customer readCustomer(int id) throws SQLException {
 
-   // TODO
-   public void updateCustomer(Customer customer) throws SQLException {
-      return;
-   }
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		Customer customer = new Customer();
+		Person person = new Person();
 
-   private static class SQL {
-      static final String INSERT_ONE
-              = "INSERT INTO customer(id, standing) VALUES (?, ?)";
+		try {
+			st = con.get().prepareStatement(SQL.SELECT_ONE);
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			customer = new Customer();
+			person = new Person();
 
-      // TODO
-      static final String SELECT_ONE = "";
+			while (rs.next()) {
+				person.setId(id);
+				customer.setStanding(rs.getBoolean("standing"));
+				person.setFirstName(rs.getString("first_name"));
+				person.setLastName(rs.getString("last_name"));
+				person.setStreet(rs.getString("street"));
+				// person.setPostalCode(rs.getShort("postal_code"));
+				// City
+				person.setPhone(rs.getInt("phone"));
+				person.setEmail(rs.getString("email"));
+			}
+			return customer;
 
-      // TODO
-      static final String UPDATE_ONE = "";
-   }
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (st != null) {
+				st.close();
+			}
+		}
+	}
+
+	// TODO
+	public void updateCustomer(Customer customer) throws SQLException {
+		return;
+	}
+
+	private static class SQL {
+		static final String INSERT_ONE = "INSERT INTO customer(id, standing) VALUES (?, ?)";
+
+		// TODO
+		static final String SELECT_ONE = "SELECT id, standing, first_name, last_name, street, postal_code, phone, email"
+				+ "FROM customer "
+				+ "JOIN person"
+				+ "on id.customer = id.person";
+
+		// TODO
+		static final String UPDATE_ONE = "";
+	}
 }
