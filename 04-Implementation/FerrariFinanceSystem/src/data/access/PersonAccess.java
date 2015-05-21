27b7862dfe3c141ea13ead90342858ed91a839ea
NiemannLouise <Niemannlouise@gmail.com>
2015-05-21 13:03:56 +0200

    DataAccess done på Person og Customer

--- 04-Implementation/FerrariFinanceSystem/src/data/access/PersonAccess.java
+++ 04-Implementation/FerrariFinanceSystem/src/data/access/PersonAccess.java
@@ -1,10 +1,10 @@
 package data.access;
 
-import domain.Person;
-import util.jdbc.ConnectionHandler;
-
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import util.jdbc.ConnectionHandler;
+import domain.Person;
 
 public class PersonAccess {
    private ConnectionHandler con;
@@ -26,10 +26,31 @@ public class PersonAccess {
       }
    }
 
+	public void updatePerson(Person person) throws SQLException {
+		try(PreparedStatement st = con.get().prepareStatement(SQL.UPDATE_ONE)) {
+			st.setString(1, person.getFirstName());
+			st.setString(2, person.getLastName());
+			st.setString(3, person.getStreet());
+			st.setInt(4, person.getPostalCode().getPostalCode());
+			st.setInt(5, person.getPhone());
+			st.setString(6, person.getEmail());
+		}
+		return;
+	}
+   
    private static class SQL {
       static final String INSERT_ONE
               = "INSERT INTO person"
               + "(id, first_name, last_name, street, postal_code, phone, email)"
               + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+      
+	static final String UPDATE_ONE = "UPDATE person"
+				+ "SET first_name = ?,"
+				+ "last_name = ?,"
+				+ "street = ?,"
+				+ "postal_code = ?,"
+				+ "phone = ?,"
+				+ "email = ?"
+				+ "WHERE id = ?";
    }
 }
