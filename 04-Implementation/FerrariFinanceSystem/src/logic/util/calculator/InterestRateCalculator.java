ddaa7595bea73d32ac93b6ec72e4106392b789c8
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-05-27 00:04:35 +0200

    Pass TC-17

--- 04-Implementation/FerrariFinanceSystem/src/logic/util/calculator/InterestRateCalculator.java
+++ 04-Implementation/FerrariFinanceSystem/src/logic/util/calculator/InterestRateCalculator.java
@@ -5,6 +5,9 @@ import com.ferrari.finances.dk.rki.Rating;
 public class InterestRateCalculator {
    double computeInterestRate(double overnightRate, Rating creditRating,
                               double downPaymentPct, int term) {
+      if (term <= 0)
+         throw new IllegalArgumentException("Term must be > 0");
+
       return 0;
    }
 }
