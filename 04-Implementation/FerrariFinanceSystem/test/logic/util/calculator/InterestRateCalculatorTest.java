6f4c1920e720e6195c6929aec375de60ebc2e367
Karsten Heino Karlsen <k.heino.karlsen@gmail.com>
2015-05-27 01:27:37 +0200

    Impl TC-13; passed

--- 04-Implementation/FerrariFinanceSystem/test/logic/util/calculator/InterestRateCalculatorTest.java
+++ 04-Implementation/FerrariFinanceSystem/test/logic/util/calculator/InterestRateCalculatorTest.java
@@ -177,6 +177,19 @@ public class InterestRateCalculatorTest {
       assertEquals(expected, actual, DELTA);
    }
 
+   @Test // TC-13
+   public void testOvernight1PctCreditADown49Point99PctTerm37Gives4PctRate() {
+      double overnightRate = 0.01;
+      Rating creditRating = Rating.A;
+      double downPaymentPct = 0.4999;
+      int term = 37;
+
+      double expected = 0.04;
+      double actual = calc.computeInterestRate(
+              overnightRate, creditRating, downPaymentPct, term);
+      assertEquals(expected, actual, DELTA);
+   }
+
    @Test // TC-16
    public void testDownPaymentPctBelow20ThrowsIllegalArgumentException() {
       thrown.expect(IllegalArgumentException.class);
